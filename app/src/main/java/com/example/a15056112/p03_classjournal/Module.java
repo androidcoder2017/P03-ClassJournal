c32450e5c10608b96a84453bd45f49dda7fecc8b
Marcus Chong You Long <mugurox@gmail.com>
2017-05-04 16:38:24 +0800

    Marcus

---
+++ app/src/main/java/com/example/a15056112/p03_classjournal/Module.java
@@ -0,0 +1,23 @@
+package com.example.a15056112.p03_classjournal;
+
+/**
+ * Created by 15004543 on 4/5/2017.
+ */
+
+public class Module {
+    private String code;
+    private String name;
+
+    public Module(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
